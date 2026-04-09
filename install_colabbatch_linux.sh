555159f73cc933a3ab80c62487d6f9b749f507bb
matteo-cagiada <39051329+matteo-cagiada@users.noreply.github.com>
2026-04-09 17:04:44 +0100

    fix_installation
    fix version of setuptools to 80.9.0 to avoid errors during installation.

--- install_colabbatch_linux.sh
+++ install_colabbatch_linux.sh
@@ -17,7 +17,7 @@ export PATH="${COLABFOLDDIR}/conda/condabin:${PATH}"
 conda update -n base conda -y
 conda create -p "$COLABFOLDDIR/colabfold-conda" -c conda-forge -c bioconda \
     git python=3.10 openmm==7.7.0 pdbfixer \
-    kalign2=2.04 hhsuite=3.3.0 mmseqs2=15.6f452 -y
+    kalign2=2.04 hhsuite=3.3.0 mmseqs2=15.6f452 setuptools=80.9.0  -y
 conda activate "$COLABFOLDDIR/colabfold-conda"
 
 # install ColabFold and Jaxlib
